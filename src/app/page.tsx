e27757efb592815986cba859b6eb788f97715773
johncorbett2 <corbett.john.j@gmail.com>
2025-05-23 00:32:28 -0400

    changed the title of the site

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import ReportList from "@/components/ReportList";
 export default function Home() {
   return (
     <main className="min-h-screen bg-gray-50">
-      <h1 className="text-center text-2xl font-bold my-6">Roach Reports 🪳</h1>
+      <h1 className="text-center text-2xl font-bold my-6">Roach Motel</h1>
       <ReportForm />
       <hr className="my-4" />
       <ReportList />
